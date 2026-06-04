1514cb3e55dd908c02365422b0f0b8872e389011
Jasmin Neveu <neveu.jasmin5@gmail.com>
2026-06-04 15:24:38 -0400

    plotly hover text display

--- code/p_dendrogram.R
+++ code/p_dendrogram.R
@@ -6,7 +6,7 @@ library(fastcluster)
 library(PCIdep)
 library(scales)
 
-build.data <- function(X,kmax = nrow(X), U = NULL, Sigma = NULL, Y = NULL, UY = NULL, precUY = NULL, linkage = 'ward.D', hcl = NULL, ndraws = 2000, sample_split = FALSE, nY = NULL, return_Sigma = FALSE, return_X_clus = FALSE, dismat = NULL,parallel_config=NULL){
+build.data <- function(X,kmax = nrow(X)-5, U = NULL, Sigma = NULL, Y = NULL, UY = NULL, precUY = NULL, linkage = 'ward.D', hcl = NULL, ndraws = 2000, sample_split = FALSE, nY = NULL, return_Sigma = FALSE, return_X_clus = FALSE, dismat = NULL,parallel_config=NULL){
   n <- nrow(X)
   if(kmax < 1 || kmax > n - 1){
     stop("kmax must be an integer between 1 and n - 1.")
@@ -16,7 +16,8 @@ build.data <- function(X,kmax = nrow(X), U = NULL, Sigma = NULL, Y = NULL, UY = 
 }
 
 
-plot.pdendrogram <- function(data_pvalues,labels_pvalues=FALSE,treshold = 0.05,low=NULL,mid=NULL,high=NULL){
+plot.pdendrogram <- function(hcl,data_pvalues,labels_pvalues=FALSE,treshold = 0.05,low=NULL,mid=NULL,high=NULL,groups=NULL,groups_labels=NULL){
+  # check paremeters
   if(treshold < 0 || treshold > 1){
     warning("treshold must be an integer between 0 and 1.")
   }
@@ -25,30 +26,25 @@ plot.pdendrogram <- function(data_pvalues,labels_pvalues=FALSE,treshold = 0.05,l
   if(partial_defined){ # si low ou mid ou high est renseigné, alors tous doivent l'être sinon c'est mal défini
     stop("Arguments 'low', 'mid' and 'high' must be provided together (either all three or none).") 
   }
-
   if(!is.null(low) && !is.null(mid) && !is.null(high)){
     colours = c(low,mid,high)
   }else{
     colours = c("#B2182B","#D9C27A","#2166AC") #option 3
   }
 
-
   p_floor <- 2.2e-16
   breaks <- c(p_floor,treshold, 1)
   labels <- c(expression("<10"^-16), as.character(treshold), "1")
   vals <- rescale(log10(c(p_floor,treshold, 1)))
 
+  # plot branches
   p <- ggplot() +
     geom_segment(
       data= data_pvalues$segments,
-      aes(x = x, y = y, xend = xend, yend = yend, colour = pval) 
+      aes(x = x, y = y, xend = xend, yend = yend, colour = pval,text="") 
     ) + 
     labs(title="p-dendrogram",x = "Observations",y="Merging distances") + 
     scale_color_gradientn(
-      # colours = c("#B2182B","#F4D35E","#2166AC"), #option 1
-      # colours = c("#C51B7D","#E9D8A6","#008B8B"), #option 2
-      # colours = c("#B2182B","#D9C27A","#2166AC"), #option 3
-      # colours = c("#D55E00","#F0E442","#0072B2"), #option 4,
       colours = colours,
       values  = vals,
       trans   = "log10",
@@ -60,53 +56,58 @@ plot.pdendrogram <- function(data_pvalues,labels_pvalues=FALSE,treshold = 0.05,l
     ) +
     theme_minimal()
 
+  # plot labels
+
+
+
   if(labels_pvalues){
-    p <- p + 
-    geom_text(
-      #TODO: gerer pbs pvalues < treshold plotly
-      data  = data_pvalues$merge_points %>% filter(label < treshold),
-      aes(x = x_mid, y = y, label = label),
-      vjust = 1.5,
-      size  = 3,
-    ) 
+    # add groups labels
+    if(!is.null(groups)){
+      if (is.null(groups_labels)) {
+        groups_labels <- paste0("group", seq_along(groups))
+      }
+      if (length(groups_labels) != length(groups)) {
+        stop("groups_labels must have the same length as groups.")
+      }
+    }
+
+    data_pvalues <- labels.groups(data_pvalues,hcl,groups,groups_labels)
+    df <- data_pvalues$merge_points 
+    data_pvalues$merge_points$hover_text <- vapply(
+      seq_len(nrow(df
+      )),
+      function(i) {
+        row <- df[i, , drop = TRUE]
+        txt <- paste0("\n size = ",row[["cluster_size"]])
+
+        for(label in groups_labels) {
+          txt <- paste0(
+            txt,
+            "\n",
+            label, ": ",
+            row[[paste0(label, "_class")]],
+            " (",
+            round(100 * as.numeric(row[[paste0(label, "_prop")]]), 1),
+            "%)"
+          )
+        }
+        txt
+      },
+      character(1)
+    )
+
+    p <- p +
+      geom_text(
+        data = data_pvalues$merge_points %>% filter(!is.null(label)),
+        aes(
+          x = x_mid,
+          y = y,
+          label = label,
+          text=  paste0("p-value = ",label, "\n k = ", k,hover_text)
+        ),
+        vjust = 1.5,
+        size  = 3
+      )
   }
   p
 }
-
-# rm(list=ls())
-# library(fastcluster)
-# library(ggplot2)
-# data <- data.frame(
-#   x = c(1,2,5,5.6,6),
-#   y = c(2,2.5,4.7,5,4)
-# )
-#
-# ggplot(data=data,aes(x=x,y=y))+geom_point()
-# hcl <- hclust(dist(data,method="euclidean"),method="ward.D")
-# plot(hcl)
-#
-
-get.individuals.merged.clusters <- function(hcl, k) {
-  n <- length(hcl$order)
-  if (k < 1 || k > n - 1) {
-    stop("k must be an integer between 1 and n - 1.")
-  }
-  merge_step <- n - (k - 1)
-  c1_internal <- hcl$merge[merge_step, 1] 
-  c2_internal <- hcl$merge[merge_step, 2]
-  get_individuals <- function(id) {
-    if (id < 0) { # cas d'un indiv
-      return(-id)
-    } else { # cas d'un cluster
-      left  <- get_individuals(hcl$merge[id, 1])
-      right <- get_individuals(hcl$merge[id, 2])
-      return(c(left, right))
-    }
-  }
-  
-  individuals_1 <- get_individuals(c1_internal)
-  individuals_2 <- get_individuals(c2_internal)
-  
-  return(list(unname(individuals_1), unname(individuals_2)))
-}
-
--- code/util_dendrogram.R
+++ code/util_dendrogram.R
@@ -20,12 +20,16 @@ get.merged.clusters <- function(hcl,k){
   c(unname(c1),unname(c2))
 }
 
-
-compute.pvals <- function(hcl, X, U, Sigma, Y, UY, precUY, linkage, ndraws, sample_split, nY, return_Sigma, return_X_clus, dismat, kmax,parallel_config) {
+# TODO: nb minimal pts dans chaque clusters genre 30/50 pts.
+compute.pvals <- function(hcl, X, U, Sigma, Y, UY, precUY, linkage, ndraws, sample_split, nY, return_Sigma, return_X_clus, dismat, kmax,parallel_config,min_pts = 30) {
   k_seq <- 2:kmax
 
   run_one_k <- function(k) {
     clusters <- get.merged.clusters(hcl, k)
+    #if(){
+    #warning(....)
+    #return (NA)
+    #}
     hc_test  <- PCIdep::test.clusters.hc(
       X = X, U = U, Sigma = Sigma, Y = Y,
       UY = UY, precUY = precUY, linkage = linkage,
@@ -63,19 +67,82 @@ get.data <- function(hcl,X, U,Sigma , Y , UY , precUY  , linkage,ndraws, sample_
   hcldata$segments$pval[1:length(pvec_rep)] <- pvec_rep
 
   # ------Add labels pvalues ------
-  merge_points <- hcldata$segments %>% #
+  merge_points <- hcldata$segments %>%
     filter(y == yend) %>%
     group_by(y) %>%
+    #.groups ?
     summarise(x_min = min(x), x_max = max(x)) %>%
     arrange(desc(y)) %>%
     mutate(
       x_mid = (x_min + x_max) / 2,
+      k = row_number()+1,
       label = c(signif(pvec,3), rep(NA, n() - length(pvec)))
     )
   hcldata$merge_points <- merge_points
   return(hcldata)
 }
 
+get.individuals.merged.clusters <- function(hcl, k) {
+  n <- length(hcl$order) 
+  if (length(k) != 1 || is.na(k)) {
+    stop("k must be a single integer.")
+  }
+  if (k < 1 || k > n) {
+    stop("k must be an integer between 1 and n - 1.")
+  }
+  merge_step <- n - (k-1)
+  c1_internal <- hcl$merge[merge_step, 1] 
+  c2_internal <- hcl$merge[merge_step, 2]
+  get_individuals <- function(id) {
+    if (id < 0) { # cas d'un indiv
+      return(-id)
+    } else { # cas d'un cluster
+      left  <- get_individuals(hcl$merge[id, 1])
+      right <- get_individuals(hcl$merge[id, 2])
+      return(c(left, right))
+    }
+  }
+  
+  individuals_1 <- get_individuals(c1_internal)
+  individuals_2 <- get_individuals(c2_internal)
+  
+  return(list(unname(individuals_1), unname(individuals_2)))
+}
 
+get.individuals <- function(hcl,k){
+  indiv <- get.individuals.merged.clusters(hcl,k+1)
+  c(indiv[[1]],indiv[[2]])
+}
 
+labels.groups <- function(data_pvalues,hcl,groups, groups_labels) {
 
+  clusters_list <- lapply(data_pvalues$merge_points$k, function(k) {
+    cl <- get.individuals.merged.clusters(hcl, k)
+    unique(c(cl[[1]], cl[[2]]))
+  })
+  cluster_sizes <- vapply(clusters_list, length, numeric(1))
+
+  for (i in seq_along(groups)) {
+
+    group <- groups[[i]]
+    label_grp <- groups_labels[i]
+
+    tmp <- lapply(clusters_list, function(indiv) {
+
+      tab <- table(group[indiv])
+
+      list(
+        class = names(tab)[which.max(tab)],
+        prop  = max(tab) / sum(tab)
+      )
+    })
+
+    data_pvalues$merge_points[[paste0(label_grp, "_class")]] <-
+      vapply(tmp, `[[`, character(1), "class")
+
+    data_pvalues$merge_points[[paste0(label_grp, "_prop")]] <-
+      vapply(tmp, `[[`, numeric(1), "prop")
+  }
+  data_pvalues$merge_points$cluster_size <- cluster_sizes
+  return(data_pvalues)
+}
